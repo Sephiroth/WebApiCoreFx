40e51670b154e118dc5fbda4f9d0fe0a967238ee
lutao <lt274660277@outlook.com>
2019-09-14 22:15:09 +0800

    补Redis包

---
+++ DBLayer/Redis/RedisUtil.cs
@@ -0,0 +1,26 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBLayer.Redis
+{
+    public class RedisUtil
+    {
+        public RedisUtil() { }
+
+        public PooledRedisClientManager CreateRedis(string[] readWriteHosts, string[] readOnlyHosts, string pwd, long db = 0)
+        {
+            PooledRedisClientManager client = new PooledRedisClientManager(readWriteHosts, readOnlyHosts, new RedisClientManagerConfig
+            {
+                MaxWritePoolSize = 10000, // “写”链接池链接数 
+                MaxReadPoolSize = 100, // “读”链接池链接数 
+                AutoStart = true,
+                DefaultDb = db
+            });
+            return client;
+        }
+
+
+    }
+}
