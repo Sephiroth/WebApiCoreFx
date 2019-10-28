a8e3aaf7ab21bd3db12069f7a765d9a34897abcf
lutao <274660277@qq.com>
2019-10-28 09:02:17 +0800

    +SqlSugar操作接口

---
+++ DotnetCore3WebApi/IRepository/SqlSugarInterface/IRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace IRepository.SqlSugarInterface
+{
+    public interface IRepository<T> where T : class, new()
+    {
+        Task<bool> AddEntity(T t);
+
+        Task<bool> AddEntity(List<T> list);
+
+        Task<bool> UpdateEntity(T t, Expression<Func<T, object>> updateColumns, Expression<Func<T, bool>> conditions);
+
+        Task<bool> UpdateEntity(List<T> list, Expression<Func<T, object>> updateColumns, Expression<Func<T, bool>> conditions);
+
+        Task<bool> UpdateEntityByIgnore(T t, Expression<Func<T, object>> ignoreColumns, Expression<Func<T, bool>> conditions);
+
+        Task<bool> UpdateEntityByIgnore(List<T> list, Expression<Func<T, object>> ignoreColumns, Expression<Func<T, bool>> conditions);
+
+        Task<bool> DeleteEntity(Expression<Func<T, bool>> expression);
+
+        Task<List<T>> GetList(string sql, int firstRow, int pageSize, string orderStr);
+
+        Task<List<T>> GetList(Expression<Func<T, bool>> expression, int firstRow, int pageSize, string orderStr);
+
+        Task<T> GetEntity(Expression<Func<T, bool>> expression);
+
+        Task<T> GetEntity(string sql);
+
+        Task<int> CountEntity(Expression<Func<T, bool>> expression);
+    }
+}
---
+++ DotnetCore3WebApi/SqlSugar.Repository/ImplBase/SqlSugarRepository.cs
@@ -0,0 +1,144 @@
+﻿using IRepository.SqlSugarInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace SqlSugar.Repository.ImplBase
+{
+    public class SqlSugarRepository<T> : IRepository<T> where T : class, new()
+    {
+        public async Task<bool> AddEntity(T t)
+        {
+            bool rs = false;
+            using (SqlSugarClient db = SqlClient.GetInstance())
+            {
+                rs = await db.Insertable(t).ExecuteCommandAsync() == 1;
+            }
+            return rs;
+        }
+
+        public async Task<bool> AddEntity(List<T> list)
+        {
+            bool rs = false;
+            using (SqlSugarClient db = SqlClient.GetInstance())
+            {
+                try
+                {
+                    db.BeginTran();
+                    rs = await db.Insertable(list).ExecuteCommandAsync() == list.Count;
+                    if (rs)
+                        db.CommitTran();
+                    else
+                        db.RollbackTran();
+                }
+                catch (Exception exp)
+                {
+                    db.RollbackTran();
+                    throw exp;
+                }
+            }
+            return rs;
+        }
+
+        public async Task<int> CountEntity(Expression<Func<T, bool>> expression)
+        {
+            int sum = 0;
+            using (SqlSugarClient db = SqlClient.GetInstance())
+            {
+                sum = await db.Queryable<T>().Where(expression).CountAsync();
+            }
+            return sum;
+        }
+
+        public async Task<bool> DeleteEntity(Expression<Func<T, bool>> expression)
+        {
+            bool rs = false;
+            using (SqlSugarClient db = SqlClient.GetInstance())
+            {
+                rs = await db.Deleteable(expression).ExecuteCommandAsync() == 1;
+            }
+            return rs;
+        }
+
+        public async Task<T> GetEntity(Expression<Func<T, bool>> expression)
+        {
+            T t = null;
+            using (SqlSugarClient db = SqlClient.GetInstance())
+            {
+                t = await db.Queryable<T>().Where(expression).FirstAsync();
+            }
+            return t;
+        }
+
+        public async Task<T> GetEntity(string sql)
+        {
+            T t = null;
+            using (SqlSugarClient db = SqlClient.GetInstance())
+            {
+                t = await db.SqlQueryable<T>(sql).FirstAsync();
+            }
+            return t;
+        }
+
+        public async Task<List<T>> GetList(string sql, int firstRow, int pageSize, string orderStr)
+        {
+            List<T> list = null;
+            using (SqlSugarClient db = SqlClient.GetInstance())
+            {
+                list = await db.SqlQueryable<T>(sql).OrderBy(orderStr).ToPageListAsync(firstRow, pageSize);
+            }
+            return list;
+        }
+
+        public async Task<List<T>> GetList(Expression<Func<T, bool>> expression, int firstRow, int pageSize, string orderStr)
+        {
+            List<T> list = null;
+            using (SqlSugarClient db = SqlClient.GetInstance())
+            {
+                list = await db.Queryable<T>().Where(expression).OrderBy(orderStr).ToPageListAsync(firstRow, pageSize);
+            }
+            return list;
+        }
+
+        public async Task<bool> UpdateEntity(T t, Expression<Func<T, object>> updateColumns, Expression<Func<T, bool>> conditions)
+        {
+            bool rs = false;
+            using (SqlSugarClient db = SqlClient.GetInstance())
+            {
+                rs = await db.Updateable(t).UpdateColumns(updateColumns).Where(conditions).ExecuteCommandAsync() == 1;
+            }
+            return rs;
+        }
+
+        public async Task<bool> UpdateEntity(List<T> list, Expression<Func<T, object>> updateColumns, Expression<Func<T, bool>> conditions)
+        {
+            bool rs = false;
+            using (SqlSugarClient db = SqlClient.GetInstance())
+            {
+                rs = await db.Updateable(list).UpdateColumns(updateColumns).Where(conditions).ExecuteCommandAsync() == list.Count;
+            }
+            return rs;
+        }
+
+        public async Task<bool> UpdateEntityByIgnore(T t, Expression<Func<T, object>> ignoreColumns, Expression<Func<T, bool>> conditions)
+        {
+            bool rs = false;
+            using (SqlSugarClient db = SqlClient.GetInstance())
+            {
+                rs = await db.Updateable(t).IgnoreColumns(ignoreColumns).Where(conditions).ExecuteCommandAsync() == 1;
+            }
+            return rs;
+        }
+
+        public async Task<bool> UpdateEntityByIgnore(List<T> list, Expression<Func<T, object>> ignoreColumns, Expression<Func<T, bool>> conditions)
+        {
+            bool rs = false;
+            using (SqlSugarClient db = SqlClient.GetInstance())
+            {
+                rs = await db.Updateable(list).IgnoreColumns(ignoreColumns).Where(conditions).ExecuteCommandAsync() == list.Count;
+            }
+            return rs;
+        }
+    }
+}
