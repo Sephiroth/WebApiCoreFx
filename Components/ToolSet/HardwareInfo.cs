88497e06dfd64ea1a4f89db6c5bb489b54336fb3
lt <lt274660277@outlook.com>
2020-09-18 14:02:02 +0800

    获取电脑硬件id信息

---
+++ Components/ToolSet/HardwareInfo.cs
@@ -0,0 +1,111 @@
+﻿using System.Management;
+
+namespace ToolSet
+{
+    public class HardwareInfo
+    {
+        private static string macAddress;
+        private static string cpuSerialNumber;
+        private static string boardSerialNumber;
+        private static string hardDiskNumber;
+
+        /// <summary>
+        /// 获取网卡mac地址
+        /// </summary>
+        /// <returns></returns>
+        public static string GetMacinfo()
+        {
+            if (string.IsNullOrEmpty(macAddress))
+            {
+                string mac = string.Empty;
+                try
+                {
+                    using ManagementClass mc = new ManagementClass("Win32_NetworkAdapterConfiguration");
+                    using ManagementObjectCollection moc = mc.GetInstances();
+                    foreach (ManagementObject mo in moc)
+                    {
+                        bool? ipEnabled = mo["IPEnabled"] as bool?;
+                        if (ipEnabled.HasValue && ipEnabled.Value)
+                        {
+                            mac = $"{mac}{mo["MacAddress"]}";
+                            break;
+                        }
+                    }
+                    macAddress = mac;
+                }
+                catch { }
+            }
+            return macAddress;
+        }
+
+        /// <summary>
+        /// 获取Cpu序列号
+        /// </summary>
+        /// <returns></returns>
+        public static string GetCpuSerialNumber()
+        {
+            if (string.IsNullOrEmpty(cpuSerialNumber))
+            {
+                try
+                {
+                    using ManagementClass mc = new ManagementClass("Win32_Processor");
+                    using ManagementObjectCollection moc = mc.GetInstances();
+                    foreach (ManagementObject mo in moc)
+                    {
+                        cpuSerialNumber = mo.Properties["ProcessorId"].Value?.ToString();
+                        break;
+                    }
+                }
+                catch { }
+            }
+            return cpuSerialNumber;
+        }
+
+        /// <summary>
+        /// 获取主板序列号
+        /// </summary>
+        /// <returns></returns>
+        public static string GetBoardSerialNumber()
+        {
+            if (string.IsNullOrEmpty(boardSerialNumber))
+            {
+                try
+                {
+                    using ManagementClass mc = new ManagementClass("Win32_BaseBoard");
+                    using ManagementObjectCollection moc = mc.GetInstances();
+                    foreach (ManagementObject mo in moc)
+                    {
+                        boardSerialNumber = mo.Properties["SerialNumber"].Value?.ToString();
+                        break;
+                    }
+                }
+                catch { }
+            }
+            return boardSerialNumber;
+        }
+
+        /// <summary>
+        /// 获取硬盘序列号
+        /// </summary>
+        /// <returns></returns>
+        public static string GetHardDiskSerialNumber()
+        {
+            if (string.IsNullOrEmpty(hardDiskNumber))
+            {
+                try
+                {
+                    using ManagementClass mc = new ManagementClass("Win32_PhysicalMedia");
+                    using ManagementObjectCollection moc = mc.GetInstances();
+                    foreach (ManagementObject mo in moc)
+                    {
+                        hardDiskNumber = mo.Properties["SerialNumber"].Value?.ToString();
+                        break;
+                    }
+                }
+                catch { }
+            }
+            return hardDiskNumber;
+        }
+
+    }
+}
--- Components/ToolSet/OrderedGuid.cs
+++ Components/ToolSet/OrderedGuid.cs
@@ -1,20 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading;
 
 namespace ToolSet
 {
     public class OrderedGuid
     {
-        public static short Order { get; set; }
-
         /// <summary>
-        /// 根据时间 + 指定Order + 随机数生成
+        /// 用于生产id的唯一锁
+        /// </summary>
+        private static readonly object lockObj = new object();
+        /// <summary>
+        /// 计算guid生成的起始日期
+        /// </summary>
+        public static DateTime? StartDate { get; set; }
+        public static short Order { get; set; }
+        private static long lastTicks;
+
+        #region 有序唯一GUID
+        /// <summary>
+        /// 根据时间 + 指定Order + 随机数生成 (mysql字符串识别顺序:左->右)
         /// </summary>
         /// <param name="order">指定主机序列</param>
         /// <returns></returns>
-        public static Guid Generate(short? order = null) //mysql(->从左向右顺序)
+        public static Guid Generate(short? order = null)
         {
             order ??= Order;
             byte[] arr = new byte[16];
@@ -27,34 +34,113 @@ namespace ToolSet
             return new Guid(arr);
         }
 
+        /// <summary>
+        /// guid生成UInt64
+        /// </summary>
+        /// <param name="guid"></param>
+        /// <returns></returns>
+        public static UInt64 GenerateUint64(Guid? guid = null)
+        {
+            Guid id;
+            if (guid.HasValue)
+                id = guid.Value;
+            else
+                id = Generate(Order);
+
+            byte[] arr = id.ToByteArray();
+            byte[] newArr = new byte[arr.Length];
+            Array.Copy(arr, 0, newArr, 8, 8);
+            newArr[0] = arr[6];
+            newArr[1] = arr[7];
+            newArr[2] = arr[4];
+            newArr[3] = arr[5];
+            newArr[4] = arr[0];
+            newArr[5] = arr[1];
+            newArr[6] = arr[2];
+            newArr[7] = arr[3];
+            return BitConverter.ToUInt64(newArr, 0);
+        }
+
         public static byte[] TicksToBytes()
         {
-            long value = DateTime.Now.Ticks;
+            long currentTicks = GetTimeTick();
             byte[] arr = new byte[8];
-            arr[0] = (byte)(value >> 0x20);
-            arr[1] = (byte)(value >> 0x28);
-            arr[2] = (byte)(value >> 0x30);
-            arr[3] = (byte)(value >> 0x38);
-            arr[4] = (byte)(value >> 0x10);
-            arr[5] = (byte)(value >> 0x18);
-            arr[6] = (byte)value;
-            arr[7] = (byte)(value >> 8);
+            arr[0] = (byte)(currentTicks >> 0x20);
+            arr[1] = (byte)(currentTicks >> 0x28);
+            arr[2] = (byte)(currentTicks >> 0x30);
+            arr[3] = (byte)(currentTicks >> 0x38);
+            arr[4] = (byte)(currentTicks >> 0x10);
+            arr[5] = (byte)(currentTicks >> 0x18);
+            arr[6] = (byte)currentTicks;
+            arr[7] = (byte)(currentTicks >> 8);
             return arr;
         }
+
+        public static Int64 GetTimeTick()
+        {
+            long currentTicks = DateTime.Now.Ticks;
+            lock (lockObj)
+            {
+                while (true)
+                {
+                    if (currentTicks > lastTicks)
+                    {
+                        lastTicks = currentTicks;
+                        break;
+                    }
+                    currentTicks = DateTime.Now.Ticks;
+                }
+                if (StartDate != null)
+                {
+                    if (StartDate >= DateTime.Today)
+                    {
+                        throw new ArgumentException("StartDate不允许大于当前机器日期");
+                    }
+                    currentTicks -= StartDate.Value.Ticks;
+                }
+            }
+            return currentTicks;
+        }
+        #endregion
+
+
+        #region 雪花算法
+        private static readonly object snowObj = new object();
+        private static long twepoch = 687888001020L; //唯一时间，这是一个避免重复的随机量，自行设定不要大于当前时间戳
+        private static long workerId=1;
+        private static readonly int workerIdBits = 4; //机器码字节数。4个字节用来保存机器码(定义为Long类型会出现，最大偏移64位，所以左移64位没有意义)
+        private static int sequenceBits = 10; //计数器字节数，10个字节用来保存计数码
+        private static int workerIdShift = sequenceBits; //机器码数据左移位数，就是后面计数器占用的位数
+        private static int timestampLeftShift = sequenceBits + workerIdBits; //时间戳左移动位数就是机器码和计数器总字节数
+        private static long sequence = 0L;
+
+        public static long NextId()
+        {
+            long nextId = 0;
+            lock (snowObj)
+            {
+                long timestamp = GetTimeTick();
+                nextId = (timestamp - (StartDate.HasValue ? StartDate.Value.Ticks : 0) << timestampLeftShift) | workerId << workerIdShift | sequence;
+            }
+            return nextId;
+        }
+        #endregion
 
 
         static void Main(string[] args)
         {
             OrderedGuid.Order = 256;
-            Random random = new Random();
+            OrderedGuid.StartDate = new DateTime(1970, 1, 1);
+
             for (int i = 0; i < 100; i++)
             {
-                int a = random.Next(50, 500);
-                var rs = Generate();
-                Console.WriteLine($"{rs.ToString()}     {a }");
-                Thread.Sleep(a);
+                //Guid rs = Generate();
+                //var a = GenerateUint64(rs);
+                //Console.WriteLine($"{a} - {rs}");
+                Console.WriteLine(NextId());
             }
             Console.ReadLine();
+
         }
     }
 }
