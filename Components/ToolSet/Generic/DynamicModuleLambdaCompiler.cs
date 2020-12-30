9790ed95aaeabe0be44629148344041a6721f2f8
Admin <Admin@DESKTOP-PQO9TK4>
2020-12-30 17:37:32 +0800

    ToolSet添加GenericActivator<T>.Create(),高效生成泛型对象

---
+++ Components/ToolSet/Generic/DynamicModuleLambdaCompiler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection.Emit;
+
+namespace ToolSet.Generic
+{
+    /// <summary>
+    /// 动态lambda编译器
+    /// </summary>
+    class DynamicModuleLambdaCompiler
+    {
+        public static Func<T> GenerateFactory<T>() where T : new()
+        {
+            Expression<Func<T>> expr = () => new T();
+            NewExpression newExpr = (NewExpression)expr.Body;
+
+            DynamicMethod method = new DynamicMethod(
+                name: "lambda",
+                returnType: typeof(T),
+                parameterTypes: new Type[0],
+                m: typeof(DynamicModuleLambdaCompiler).Module,
+                skipVisibility: true
+                );
+
+            ILGenerator iLGen = method.GetILGenerator();
+            if (newExpr.Constructor != null)
+            {
+                iLGen.Emit(OpCodes.Newobj, newExpr.Constructor);
+            }
+            else
+            {
+                LocalBuilder temp = iLGen.DeclareLocal(typeof(T));
+                iLGen.Emit(OpCodes.Ldloca, temp);
+                iLGen.Emit(OpCodes.Initobj, newExpr.Type);
+                iLGen.Emit(OpCodes.Ldloc, temp);
+            }
+            iLGen.Emit(OpCodes.Ret);
+            return (Func<T>)method.CreateDelegate(typeof(Func<T>));
+        }
+
+    }
+}
---
+++ Components/ToolSet/Generic/GenericActivator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ToolSet.Generic
+{
+    public class GenericActivator
+    {
+        public static T CreateInstance<T>() where T : new()
+        {
+            return GenericActivatorImpl<T>.NewFunction();
+        }
+
+        private class GenericActivatorImpl<T> where T : new()
+        {
+            // 利用表达式树就会“提示”编译器不会调用 Activator.CreateInstance 
+            private static readonly Expression<Func<T>> NewExpression = () => new T();
+            internal static readonly Func<T> NewFunction = NewExpression.Compile();
+        }
+    }
+
+    /// <summary>
+    /// 高效创建泛型对象(eg: GenericActivator<T>.Create())
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public static class GenericActivator<T> where T : new()
+    {
+        public static readonly Func<T> Create = DynamicModuleLambdaCompiler.GenerateFactory<T>();
+    }
+
+    //class Program
+    //{
+    //    static void Main()
+    //    {
+    //        object obj = GetObj<object>();
+    //    }
+    //    static T GetObj<T>() where T : class, new()
+    //    {
+    //        return GenericActivator<T>.Create();
+    //    }
+    //}
+}
